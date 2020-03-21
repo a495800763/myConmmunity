09ef224ad59c5ba4097decadec8240caa4e4847d
liumq-95 <495800763@qq.com>
2020-03-21 16:46:38 +0800

    登录和已登录状态标签切换

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author Administrator
  */
@@ -25,10 +27,10 @@ public class AuthorizeController {
     @Value("${github.redirecturi}")
     private String redirectUri;
 
-
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
@@ -39,6 +41,15 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
-        return "index";
+
+        if(user!=null)
+        {
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+            //登录成功 写cookie 和session
+        }else{
+            return  "redirect:/";
+            //登录失败，重新登录
+        }
     }
 }
