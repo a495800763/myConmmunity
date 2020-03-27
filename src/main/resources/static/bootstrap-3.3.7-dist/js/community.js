bc07c7df570ccf4b23577ff8f81bb694ffdb7be9
liumq-95 <495800763@qq.com>
2020-03-27 20:56:48 +0800

    登录后回到本页面

--- src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
+++ src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
@@ -1,28 +1,33 @@
-function  post(){
+function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
     console.log(questionId);
     console.log(content);
 
     $.ajax({
-       type:"POST",
-       url:"/comment",
-        contentType:'application/json',
-       data:JSON.stringify({
-           "parentId":questionId,
-           "content":content,
-           "type":1
-       }),
-       success:function (response) {
-           if(response.code==200)
-           {
-               $("#comment_section").hide();
-           }
-           else{
-               alert(response.message);
-           }
-  console.log(response);
-       },
-       dataType:"json"
+        type: "POST",
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
+        }),
+        success: function (response) {
+            if (response.code == 200) {
+                $("#comment_section").hide();
+            } else {
+                if (response.code = 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=Iv1.d280d4d80035c843&redirect_uri=http://localhost:8887/callback&scope=user&state=1")
+                        window.localStorage.setItem("closable",true);
+                    }
+                } else {
+                    alert(response.message);
+                }
+            }
+        },
+        dataType: "json"
     });
 }
